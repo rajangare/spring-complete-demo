fc0e2880d308837f7c435745d13927fa743827b4
Rajkumar ANGARE <rajangare@gmail.com>
2020-09-06 12:01:24 +0530

    Demo project
    Signed-off-by: Rajkumar ANGARE <rajangare@gmail.com>

---
+++ src/main/java/com/demo/springcompletedemo/SpringCompleteDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springcompletedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCompleteDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCompleteDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/springcompletedemo/SpringCompleteDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springcompletedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCompleteDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
